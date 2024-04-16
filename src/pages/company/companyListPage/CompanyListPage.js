37d4cb026159e48759d2c5d4e8dc5017b20933e0
Kevin Quiercelin <kevinquiercelin@numidev.fr>
2024-04-16 22:03:04 +0200

    fix list style

--- src/pages/company/companyListPage/CompanyListPage.js
+++ src/pages/company/companyListPage/CompanyListPage.js
@@ -27,7 +27,7 @@ const CompanyListPage = ({
     indexOfLastItem
   );
 
-  const totalPages = Math.ceil(filteredListCompanies.length / itemsPerPage);
+  const totalPages = Math.ceil(filteredListCompanies.length / itemsPerPage +1);
 
   const nextPage = () => {
     setCurrentPage(currentPage + 1);
@@ -55,20 +55,18 @@ const CompanyListPage = ({
       </div>
 
       <div className="p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-50 dark:border-gray-50 flex flex-col mt-10">
-        <h1 className="text-xl font-semibold">Liste des entreprises</h1>
-        <div className="flex justify-between items-right px-5 mt-4 mb-4">
-          <a href="company/companyAddPage" className="self-end ml-auto">
-            {" "}
-            {/* Ajoute ml-auto ici */}
-            <Button
+      <div className="flex justify-between items-center px-5 mt-4 mb-5" >      
+      <h1 className="text-xl font-semibold">Liste des entreprises</h1>
+        <a href="company/companyAddPage" className="flex items-center">
+          <Button
             type="button"
             label="Ajouter"
-            style="w-70 rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            />
-          </a>
-        </div>
+            style="rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          />
+        </a>
+      </div>
 
-        <div className="overflow-auto">
+      <div className="flex flex-col">
           <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 pr-5">
             <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
               <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
@@ -146,16 +144,10 @@ const CompanyListPage = ({
             </div>
           </div>
         </div>
-        <div className="flex justify-between mt-5">
-          <button onClick={prevPage} disabled={currentPage === 1}>
-            Page précédente
-          </button>
-          <span>
-            Page {currentPage} sur {totalPages}
-          </span>
-          <button onClick={nextPage} disabled={currentPage === totalPages}>
-            Page suivante
-          </button>
+        <div class="flex justify-between mt-5">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onclick={prevPage} disabled={currentPage === 1}>Page précédente</button>
+          <span class="text-gray-700">Page {currentPage} sur {totalPages}</span>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onclick={nextPage} disabled={currentPage === totalPages}>Page suivante</button>
         </div>
       </div>
     </View>
